120c8f0936a1cfbd76b971297bb285c8b3e33ddf
RamSChoudhary <ramrit10@gmail.com>
2023-03-11 20:09:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ terraform {
       version = "3.47.0"
     }
   }
+     backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "example-organization"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "example-workspace"
+         }
+       }
+
 }
 
 provider "azurerm" {
