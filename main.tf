8d4aceb2e77fbb624db983e8067b8d933a4b5d43
RamSChoudhary <ramrit10@gmail.com>
2023-03-12 00:20:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ terraform {
 
 }
 
-provider "azurerm" {
+#provider "azurerm" {
   # Configuration options
   #tenant_id       = "6247d758-84d5-49c9-b680-d24ea85bb764"
   #subscription_id = "a20c6610-8802-4de7-91ff-dcd95bcbb16d"
-  features {
-  }
-}
+ # features {
+ # }
+#}
 
 #resource "azurerm_resource_group" "example" {
 #  name     = "example"
