340a7361308b72d3caecca211c6da634878879fc
RamSChoudhary <ramrit10@gmail.com>
2023-04-23 22:03:14 +0530

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -13,6 +13,22 @@ resource "azurerm_key_vault" "test-kv" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 
   sku_name = "standard"
 }
