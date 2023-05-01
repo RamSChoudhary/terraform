ac2201d7a5a641459bdc42b1408e39c9058480e9
RamSChoudhary <ramrit10@gmail.com>
2023-05-01 17:37:23 +0530

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -43,27 +43,26 @@ resource "azurerm_key_vault_key" "test-key" {
     "verify",
     "wrapKey",
   ]
+  
+  depends_on =[null_resource.kv-keys-add]
 }
 
+ resource "null_resource" "kv-keys-add" {
+   # Changes to any instance of the cluster requires re-provisioning
+   triggers = {
+   }
 
-# resource "null_resource" "cluster" {
-#   # Changes to any instance of the cluster requires re-provisioning
-#   triggers = {
-#     cluster_instance_ids = join(",", aws_instance.cluster.*.id)
-#   }
+   # Bootstrap script can run on any instance of the cluster
+   # So we just choose the first in this case
+   connection {
+     #host = element(aws_instance.cluster.*.public_ip, 0)
+   }
 
-#   # Bootstrap script can run on any instance of the cluster
-#   # So we just choose the first in this case
-#   connection {
-#     host = element(aws_instance.cluster.*.public_ip, 0)
-#   }
-
-#   provisioner "remote-exec" {
-#     # Bootstrap script called with private_ip of each node in the cluster
-#     inline = [
-#       "agentIP=$(curl -s https://api.ipify.org/) 
-#       az keyvault network-rule add --resource-group test-rg --name test-kv1441 --ip-address $agentIP"
-#       ,
-#     ]
-#   }
-# }
+   provisioner "remote-exec" {
+     # Bootstrap script called with private_ip of each node in the cluster
+     inline = [
+       "agentIP=$(curl -s https://api.ipify.org/);  az keyvault network-rule add --resource-group test-rg --name test-kv1441 --ip-address $agentIP"
+       ,
+     ]
+   }
+ }
