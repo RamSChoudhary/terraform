35c1b2e733d4ed7c272a9f64834964d4e2cdf60d
RamSChoudhary <ramrit10@gmail.com>
2023-05-09 16:13:32 +0530

    Update roles.tf

--- roles.tf
+++ roles.tf
@@ -1,9 +1,10 @@
 data "azurerm_billing_enrollment_account_scope" "test" {
-  billing_account_name    = "ramrit10@gmail.com"
+  billing_account_name    = "Microsoft Online Services Program"
   enrollment_account_name = "ramrit10@gmail.com"
 }
 
 resource "azurerm_subscription" "test-subs" {
   subscription_name = "Test-Subscription"
   billing_scope_id  = data.azurerm_billing_enrollment_account_scope.test.id
+  workload = "DevTest"
 }
