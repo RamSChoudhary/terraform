d850c71d8d24f03743bd6f2f9eee3542f85ac4f8
RamSChoudhary <ramrit10@gmail.com>
2023-05-09 15:44:27 +0530

    Update roles.tf

--- roles.tf
+++ roles.tf
@@ -1,9 +1,9 @@
-data "azurerm_billing_enrollment_account_scope" "example" {
+data "azurerm_billing_enrollment_account_scope" "test" {
   billing_account_name    = "1234567890"
   enrollment_account_name = "0123456"
 }
 
-resource "azurerm_subscription" "example" {
-  subscription_name = "My Example EA Subscription"
-  billing_scope_id  = data.azurerm_billing_enrollment_account_scope.example.id
+resource "azurerm_subscription" "test-subs" {
+  subscription_name = "Test-Subscription"
+  billing_scope_id  = data.azurerm_billing_enrollment_account_scope.test.id
 }
