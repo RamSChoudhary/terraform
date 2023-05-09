a126ea32b5139bb0b34306a0c700207754e551e5
RamSChoudhary <ramrit10@gmail.com>
2023-05-09 16:43:25 +0530

    Update roles.tf

--- roles.tf
+++ roles.tf
@@ -1,6 +1,6 @@
 data "azurerm_billing_enrollment_account_scope" "test" {
   billing_account_name    = "ramrit10@gmail.com"
-  enrollment_account_name = "Microsoft Online Services Program"
+  enrollment_account_name = "MS-AZR-0003P"
 }
 
 resource "azurerm_subscription" "test-subs" {
