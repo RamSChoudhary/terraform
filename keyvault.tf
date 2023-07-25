35a47b470cd327b9954ec499f9e87b3c596d4cb4
RamSChoudhary <ramrit10@gmail.com>
2023-07-25 08:12:49 +0100

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -5,55 +5,55 @@ resource "azurerm_resource_group" "test-rg" {
   location = "eastus"
 }
 
-#resource "azurerm_key_vault" "test-kv" {
-#  name                          = "test-kv1441"
-#  location                      = azurerm_resource_group.test-rg.location
-#  resource_group_name           = azurerm_resource_group.test-rg.name
-#  enabled_for_disk_encryption   = true
-#  tenant_id                     = data.azurerm_client_config.current.tenant_id
-#  soft_delete_retention_days    = 7
-#  purge_protection_enabled      = false
-#  public_network_access_enabled = false
+resource "azurerm_key_vault" "test-kv" {
+  name                          = "test-kv1441"
+  location                      = azurerm_resource_group.test-rg.location
+  resource_group_name           = azurerm_resource_group.test-rg.name
+  enabled_for_disk_encryption   = true
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = false
+  public_network_access_enabled = false
 
-#  access_policy {
-#    tenant_id = data.azurerm_client_config.current.tenant_id
-#    object_id = data.azurerm_client_config.current.object_id
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-#    key_permissions         = ["Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
-#    secret_permissions      = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
-#    certificate_permissions = ["Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"]
-#    storage_permissions     = ["Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"]
+    key_permissions         = ["Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
+    secret_permissions      = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
+    certificate_permissions = ["Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"]
+    storage_permissions     = ["Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"]
 
-#  }
+  }
 
-#  sku_name = "standard"
-
-  
-
-#  lifecycle {
-#    ignore_changes = [ public_network_access_enabled ]
- # }
-#}
-
-#resource "azurerm_key_vault_key" "test-key" {
-#  name         = "test-key"
-#  key_vault_id = azurerm_key_vault.test-kv.id
-#  key_type     = "RSA"
-#  key_size     = 2048
-
-#  key_opts = [
-#    "decrypt",
-#    "encrypt",
-#    "sign",
-#    "unwrapKey",
-#    "verify",
-#    "wrapKey",
-#  ]
+  sku_name = "standard"
 
   
 
-#  depends_on = [null_resource.kv-keys-add]
-#}
+  lifecycle {
+    ignore_changes = [ public_network_access_enabled ]
+  }
+}
+
+resource "azurerm_key_vault_key" "test-key" {
+  name         = "test-key"
+  key_vault_id = azurerm_key_vault.test-kv.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  
+
+  depends_on = [null_resource.kv-keys-add]
+}
 
 #resource "null_resource" "kv-keys-add" {
   #  # Changes to any instance of the cluster requires re-provisioning
