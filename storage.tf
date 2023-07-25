adc39a8a6c71a06ff9a6eebdfeb7a1d93b759957
RamSChoudhary <ramrit10@gmail.com>
2023-07-25 08:24:09 +0100

    Create storage.tf

---
+++ storage.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname14411"
+  resource_group_name      = "${azurerm_resource_group.test-rg.name}"
+  location                 = "${azurerm_resource_group.test-rg.location}"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
